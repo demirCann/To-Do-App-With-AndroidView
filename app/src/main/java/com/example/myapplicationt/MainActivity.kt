46fe036888d13c8c54af87ec814a7c1bb3f40fec
Demir <79748534+demirCann@users.noreply.github.com>
2024-03-19 15:29:12 +0300

    Navigation Components done.

--- app/src/main/java/com/example/myapplicationt/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationt/MainActivity.kt
@@ -1,20 +1,50 @@
 package com.example.myapplicationt
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.myapplicationt.databinding.ActivityMainBinding
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        setSupportActionBar(binding.myToolbar)
+
+
+
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
+
+
+
     }
+
+
+/*
+    override fun onSupportNavigateUp(): Boolean {
+        Log.d("onNavigateUp", "entered")
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
+ */
+
 }
